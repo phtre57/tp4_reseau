7441716ea8a604db896f3ecff2bf3b6f3520a41a
phtre57 <philippe.trepanier.1@ulaval.ca>
2017-11-24 16:58:34 -0500

    ajout de lenvoie de courriels

--- client.py
+++ client.py
@@ -15,9 +15,9 @@ class Client:
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect(self.destination)
 
-        self.showConnexionMenu()
+        self.__showConnexionMenu()
 
-    def showConnexionMenu(self):
+    def __showConnexionMenu(self):
         choice = input("Menu de connexion \n 1. Se connecter \n 2. Creer un compte \n")
 
         if choice == 1 or choice == str(1):
@@ -28,7 +28,7 @@ class Client:
 
         else:
             print("Choix invalide")
-            self.showConnexionMenu()
+            self.__showConnexionMenu()
 
     def __connexionToServer(self):
         self.username = input("Entrez votre nom d'usager: ")
@@ -38,22 +38,25 @@ class Client:
         send_msg(self.s, self.username)
         send_msg(self.s, self.password)
 
-        message = recv_msg(self.s)
+        try:
+            message = recv_msg(self.s)
 
-        if message == "passwordOk":
-            self.showMainMenu()
+            if message == "passwordOk":
+                self.__showMainMenu()
 
-        elif message == "passwordWrong":
-            print("Mauvais mot de passe")
-            self.__connexionToServer()
+            elif message == "passwordWrong":
+                print("Mauvais mot de passe")
+                self.__connexionToServer()
 
-        elif message == "notExistUsername":
-            print("Le nom d'usage rentrer ne correspond a aucun usage")
-            self.__connexionToServer()
+            elif message == "notExistUsername":
+                print("Le nom d'usage rentrer ne correspond a aucun usage")
+                self.__connexionToServer()
 
-        else:
-            print("Probleme au niveau du serveur: " + message)
-            self.__connexionToServer()
+            else:
+                print("Probleme au niveau du serveur: " + message)
+                self.__connexionToServer()
+        except Exception as ex:
+            print(ex)
 
 
     def __createUser(self):
@@ -72,24 +75,73 @@ class Client:
             print("Le mot de passe ne contient pas entre 6-12 caracteres ou au moins une lettre et un chiffre")
             self.__createPassword()
 
-        message = recv_msg(self.s)
-        if message == "userCreated":
-            print("Usage cree")
-            self.showMainMenu()
-        elif message == "usernameExists":
-            print("Usage existe deja veuillez choisir un autre nom d'usage")
-            self.__createUser()
-        elif message == "incorrectPassword":
-            self.__createPassword()
-        else:
-            print("Probleme au niveau du serveur: " + message)
-            self.showConnexionMenu()
+        try:
+            message = recv_msg(self.s)
+            if message == "userCreated":
+                print("Usage cree")
+                self.__showMainMenu()
+            elif message == "usernameExists":
+                print("Usage existe deja veuillez choisir un autre nom d'usage")
+                self.__createUser()
+            elif message == "incorrectPassword":
+                self.__createPassword()
+            else:
+                print("Probleme au niveau du serveur: " + message)
+                self.__showConnexionMenu()
+        except Exception as ex:
+            print(ex)
 
 
-    def showMainMenu(self):
+    def __showMainMenu(self):
         print("Bienvenue: " + self.username)
         choice = input("Menu principal \n 1. Envoie de courriel \n 2. Consultation de courriels \n "
                        "3. Statistiques \n 4. Quitter \n")
+
+        if choice == str(1) or choice == 1:
+            self.__sendMail()
+            self.__showMainMenu()
+        elif choice == str(2) or choice == 2:
+            print(2)
+            self.__showMainMenu()
+        elif choice == str(3) or choice == 3:
+            print(3)
+            self.__showMainMenu()
+        elif choice == str(4) or choice == 4:
+            self.username = ""
+            self.password = ""
+            self.__showConnexionMenu()
+        else:
+            print("Choix invalide")
+            self.__showMainMenu()
+
+    def __sendMail(self):
+        dest = self.__destOk()
+        subject = input("Entrez le sujet: ")
+        message = input("Entrez le message: ")
+
+        send_msg(self.s, str(3))
+        send_msg(self.s, dest)
+        send_msg(self.s, subject)
+        send_msg(self.s, message)
+        send_msg(self.s, self.username + "@reseauglo.ca")
+
+        try:
+            message = recv_msg(self.s)
+            print(message)
+        except Exception as ex:
+            print(ex)
+
+    def __destOk(self):
+        dest = input("Entrez le destinataire: ")
+        if re.search(r"[^@]+@[^@]+\.[^@]", dest):
+            return dest
+        else:
+            print("Rentrez un email valide (ex: exemple@gmail.com)")
+            self.__destOk()
+
+
+
+
 
 
 newClient = Client()
--- serveur.py
+++ serveur.py
@@ -6,6 +6,8 @@ import re
 import os
 import errno
 from hashlib import sha256
+import smtplib
+from email.mime.text import MIMEText
 
 class Server:
 
@@ -16,7 +18,13 @@ class Server:
         self.serversocket.bind(self.destination)
         self.serversocket.listen(5)
         print("Listening on port " + str(self.serversocket.getsockname()[1]))
-
+        filename = "./DESTERREUR"
+        if not os.path.exists(filename):
+            try:
+                os.makedirs(filename)
+            except OSError as ex:
+                if ex.errno != errno.EEXIST:
+                    print(ex)
         self.__listenForConnexion()
 
     def __listenForConnexion(self):
@@ -29,61 +37,127 @@ class Server:
 
     def __listen(self, s):
         try:
-            operation = recv_msg(s)
+            operation = str(recv_msg(s))
 
+            #1 connexion user
             if operation == str(1):
                 self.__connexion(s)
 
+            #2 creation user
             elif operation == str(2):
                 self.__createUser(s)
+
+            #3 send mail
+            elif operation == str(3):
+                self.__sendMail(s)
+
         except Exception as ex:
             print(ex)
 
 
     def __connexion(self, s):
-        username = recv_msg(s)
-        password = recv_msg(s)
-        hashedPassword = sha256(password.encode()).hexdigest()
-        filename = "./" + username + "/config.txt"
-        if os.path.exists(os.path.dirname(filename)):
-            try:
-                with open(filename, "r") as file:
-                    readPassword = file.readline();
-                    if str(hashedPassword) == str(readPassword):
-                        send_msg(s, "passwordOk")
-                    else:
-                        send_msg(s, "passwordWrong")
-            except OSError as ex:
-                if ex.errno != errno.EEXIST:
-                    send_msg(s, "race condition")
-        else:
-            send_msg(s, "notExistUsername")
-
+        try:
+            username = recv_msg(s)
+            password = recv_msg(s)
+            hashedPassword = sha256(password.encode()).hexdigest()
+            filename = "./" + username + "/config.txt"
+            if os.path.exists(os.path.dirname(filename)):
+                try:
+                    with open(filename, "r") as file:
+                        readPassword = file.readline();
+                        if str(hashedPassword) == str(readPassword):
+                            send_msg(s, "passwordOk")
+                        else:
+                            send_msg(s, "passwordWrong")
+                except OSError as ex:
+                    if ex.errno != errno.EEXIST:
+                        send_msg(s, "race condition")
+            else:
+                send_msg(s, "notExistUsername")
+        except Exception as ex:
+            print(ex)
 
         self.__listen(s)
 
     def __createUser(self, s):
-        username = recv_msg(s)
-        password = recv_msg(s)
-        hashedPassword = ""
-        if re.search(r"^(?=.*\d)(?=.*[a-zA-Z]).{6,12}$", password):
-            hashedPassword = sha256(password.encode()).hexdigest()
-        else:
-            send_msg(s, "incorrectPassword")
+        try:
+            username = recv_msg(s)
+            password = recv_msg(s)
+            hashedPassword = ""
+            if re.search(r"^(?=.*\d)(?=.*[a-zA-Z]).{6,12}$", password):
+                hashedPassword = sha256(password.encode()).hexdigest()
+            else:
+                send_msg(s, "incorrectPassword")
 
-        filename = "./" + username + "/config.txt"
-        if not os.path.exists(os.path.dirname(filename)):
-            try:
-                os.makedirs(os.path.dirname(filename))
-            except OSError as ex:
-                if ex.errno != errno.EEXIST:
-                    send_msg("race condition")
+            filename = "./" + username + "/config.txt"
+            if not os.path.exists(os.path.dirname(filename)):
+                try:
+                    os.makedirs(os.path.dirname(filename))
+                except OSError as ex:
+                    if ex.errno != errno.EEXIST:
+                        send_msg("race condition")
 
-            with open(filename, "w") as file:
-                file.write(hashedPassword)
-            send_msg(s, "userCreated")
-        else:
-            send_msg(s, "usernameExists")
+                with open(filename, "w") as file:
+                    file.write(hashedPassword)
+                send_msg(s, "userCreated")
+            else:
+                send_msg(s, "usernameExists")
+        except Exception as ex:
+            print(ex)
+
+        self.__listen(s)
+
+    def __sendMail(self, s):
+        try:
+            dest = recv_msg(s)
+            subject = recv_msg(s)
+            message = recv_msg(s)
+            username = recv_msg(s)
+
+            if re.search(r".+@reseauglo\.ca", dest):
+                dest1, dest2 = dest.split("@")
+                filename = "./" + dest1
+                if os.path.exists(filename):
+                    try:
+                        os.makedirs(os.path.dirname(filename + "/" + subject + ".txt"))
+
+                    except OSError as ex1:
+                        if ex1.errno != errno.EEXIST:
+                            send_msg(s, "race condition")
+                            print(ex1)
+
+                    with open(filename + "/" + subject + ".txt", "w") as file:
+                        file.write(message)
+                        send_msg(s, "mailOk")
+
+                else:
+                    try:
+                        os.makedirs(os.path.dirname("DESTERREUR/" + subject + ".txt"))
+
+                    except OSError as ex1:
+                        if ex1.errno != errno.EEXIST:
+                            send_msg(s, "race condition")
+                            print(ex1)
+
+                    with open("DESTERREUR/" + subject + ".txt", "w") as file:
+                        file.write(message)
+                        send_msg(s, "noDest")
+
+            else:
+                msg = MIMEText(message)
+                msg["FROM"] = username
+                msg["TO"] = dest
+                msg["Subject"] = subject
+                try:
+                    smtpConnection = smtplib.SMTP(host="smtp.ulaval.ca", timeout=10)
+                    smtpConnection.sendmail(username, dest, msg.as_string())
+                    smtpConnection.quit()
+                    send_msg(s, "otherDest")
+                except Exception as ex2:
+                    send_msg(s, ex2)
+
+        except Exception as ex:
+            print(ex)
 
         self.__listen(s)
 
